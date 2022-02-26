145f087d033fa89e718ba73c21a6a92b3ea71628
Daniel <dannyb829@gmail.com>
2022-02-25 22:29:13 -0500

    commit tree 08323532d062d6a8f135db611e27cfb4a1476472

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+let totalBatteries = batteryBatches.reduce((acc, i) => acc += i, 0)
+
+// const reducer = (arr) => {totalBatteries = arr.reduce((acc, i) => acc += i, 0)}
